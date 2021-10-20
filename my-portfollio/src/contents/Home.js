fb2f1663e081372a24945cdc724bd8c923bfa041
KittiphobChuaicharoensuk <kittiphob.chuaicharoensuk@gmail.com>
2021-10-20 08:41:21 +0700

    new submitted

---
+++ my-portfollio/src/contents/Home.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import Social from '../components/Social'
+import ReactTypingEffect from 'react-typing-effect'
+
+class Home extends Component{
+
+    render(){
+         return(
+            <div className="condiv home">
+            <img src="https://media1.giphy.com/media/9C1nyePnovqlpEYFMD/giphy.gif" alt="profile" className="profilepic"/>
+            <ReactTypingEffect text={["I'm Kittiphob", "I'm a web developer"]} speed={20} eraseDelay={20} className="typingeffect"/>
+            <Social/>
+        </div>
+         );
+    }
+} 
+
+export default Home;
